006057456b22b7c6a7dbf7e11916dced9d1d8d5f
paszo <paszo2008@gmail.com>
2020-07-18 17:21:58 +0200

    Added some content to make site work

---
+++ utils.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const httpStatus = require('http-status-codes');
+const contentTypes = require('./contentTypes');
+
+module.exports = {
+    getFile: (file, res) => {
+        fs.readFile(`./${file}`, (error, data) => {
+            if(error) {
+                console.log('error no such file');
+                res.writeHead(httpStatus.INTERNAL_SERVER_ERROR, contentTypes.html);
+                res.end("There was an error serving content!");
+            }
+            res.end(data);
+        });
+    }
+};
